a44e19f9e386a81b16973403ec286b59490a634c
Vinoth Raja <nvrevrgivup@gmail.com>
2020-06-16 20:04:22 +0530

    Update softmax_layer.h

--- src/softmax_layer.h
+++ src/softmax_layer.h
@@ -10,7 +10,7 @@ typedef layer contrastive_layer;
 extern "C" {
 #endif
 void softmax_array(float *input, int n, float temp, float *output);
-softmax_layer make_softmax_layer(int batch, int inputs, int groups);
+softmax_layer make_softmax_layer(int batch, int inputs, int groups, int start_idx);
 void forward_softmax_layer(const softmax_layer l, network_state state);
 void backward_softmax_layer(const softmax_layer l, network_state state);
 
